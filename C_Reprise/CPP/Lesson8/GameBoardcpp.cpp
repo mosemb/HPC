d328f85f5282eb9f175c1dcf4a357ff9ab1561b2
mosemb <mosejava@gmail.com>
2022-03-10 11:10:03 +0000

    more cpp

---
+++ C_Reprise/CPP/Lesson8/GameBoardcpp.cpp
@@ -0,0 +1,36 @@
+#include "GameBoardcpp.hpp"
+
+
+int main(void){
+    Gameboard game1;
+    // Row number 1 
+    game1.setGameSpace(0,0,'x');
+    game1.setGameSpace(0,1,'x');
+    game1.setGameSpace(0,2,'x');
+    game1.setGameSpace(0,3,'x');
+
+    // Row number 2
+    game1.setGameSpace(1,0,'x');
+    game1.setGameSpace(1,1,'-');
+    game1.setGameSpace(1,2,'x');
+    game1.setGameSpace(1,3,'-');
+
+    // Row number 3
+    game1.setGameSpace(2,0,'x');
+    game1.setGameSpace(2,1,'-');
+    game1.setGameSpace(2,2,'-');
+    game1.setGameSpace(2,3,'x');
+
+    // Row number 4
+    game1.setGameSpace(3,0,'x');
+    game1.setGameSpace(3,1,'-');
+    game1.setGameSpace(3,2,'-');
+    game1.setGameSpace(3,3,'x');
+
+    // Rows 
+    int win = game1.fourInRow();
+    game1.printInfo();
+
+    cout<< "State :"<<win << "\n";
+    
+}
---
+++ C_Reprise/CPP/Lesson8/GameBoardcpp.hpp
@@ -0,0 +1,71 @@
+#include <iostream> 
+#include <stdio.h>
+
+using namespace std;
+
+
+class Gameboard {
+
+     char gameSpace[4][4];
+public:
+    Gameboard(); //initialize the board with '-' in all 16 spaces
+    void setGameSpace(int row,int column, char value); //x,y,or '-' in each game square
+    char getGameSpace(int row,int column);
+    int fourInRow(); //four 'x's in any row 'wins'
+    void printInfo(); //print the game board in a 4x4 matrix
+
+
+};
+
+Gameboard::Gameboard(){
+
+    for(int i = 0; i<4; i++){
+        for(int j =0; j<4; j++){
+            gameSpace[i][i] = '-';
+        }
+    }
+}
+
+void Gameboard::setGameSpace(int row, int column, char value){
+
+    gameSpace[row][column] = value;
+
+}
+
+char Gameboard::getGameSpace(int row, int column){
+
+    char space = ' ';
+    for(int i = 0; i<4; i++){
+        for(int j =0; i<4; j++){
+            if(gameSpace[i][j]==gameSpace[row][column]){
+                space = gameSpace[i][j];
+            }
+        }
+    }
+
+    return space;
+}
+
+int Gameboard::fourInRow(){
+    int win = 0;
+    for(int i = 0; i<4; i++){
+        for(int j =0; j<4; j++){
+            if(gameSpace[i][j]=='x')
+            {
+                win = 1;
+            }
+        }
+    }
+    return win;
+}
+
+void Gameboard::printInfo(){
+    for(int i =0; i<4; i++){
+        for(int j=0; j<4; j++){
+            cout<<"Element at row " << i << " and Column :"<< j << " is "<< gameSpace[i][j] << "\n";
+        }
+        cout<<"\n";
+    }
+}
+
+
