0c0fbda03ab83bdbc377dd3bff28c663c52a4f40
mosemb <mosejava@gmail.com>
2022-03-31 00:38:34 +0000

    mutex

--- C_Reprise/HpcReal/Parallell_C/threads.cpp
+++ C_Reprise/HpcReal/Parallell_C/threads.cpp
@@ -1,12 +1,20 @@
 #include <thread>
 #include <iostream>
 #include <stdio.h>
+#include <mutex> // Has lock to make sure threads move in order 
 
-//using namespace std;
+using namespace std;
+mutex mtx;
 
 
 void call_from(int tid ){
+
+    // There is a little problem with our printing of the ids. They are not in order. 
+    // We need to fix this. 
+
+    mtx.lock(); // Locks the thread
     printf("Thread number %i \n",tid); 
+    mtx.unlock(); // Unlocks the thread. 
 }
 
 
@@ -14,12 +22,12 @@ void call_from(int tid ){
 int main(){
 
 
-    std::thread t[10];
+    thread t[10];
     for(int i=0; i<10; i++){
-        t[i]=std::thread(call_from,i); 
+        t[i]=thread(call_from,i); 
     }
 
-    printf("This is the main thread ... ");
+    printf("This is the main thread ... \n");
 
     // Wait for all the threads to complete 
     for(int i = 0; i<10; i++){
